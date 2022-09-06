9433ace1c0b790552ccc7b07c709a4c3bcdb4f9b
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2022-09-06 15:48:51 +0100

    scrollY and component links added

--- portfolio/components/NavBar.js
+++ portfolio/components/NavBar.js
@@ -1,106 +1,120 @@
-import React, {useState} from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
-import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import React, {useState, useEffect} from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 
 const NavBar = () => {
   const [nav, setNav] = useState(false)
+  const [shadow, setShadow] =useState(false)
 
   const handleNav = () => {
     setNav(!nav)
   }
 
+  useEffect (() =>{
+     const handleShadow =() => {
+      if (window.scrollY >=90) {
+        setShadow(true)
+      } else {
+        setShadow(false)
+      }
+     }
+     window.addEventListener('scroll', handleShadow);
+  }, [])
+
   return (
-    <div className="fixed w-full h-20 shadow-xl z-[100]">
-      <div className="flex justify-between items-center w-full px-2 2xl:px-16">
+    <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
+      <div className='flex justify-between items-center w-full px-2 2xl:px-16'>
+      <Link href='/'>
         <Image
-          src="/../public/assets/navLogo.png"
-          alt="/"
-          width="125"
-          height="50"
+          src='/../public/assets/navLogo.png'
+          alt='/'
+          width='125'
+          height='50'
         />
+      </Link>
         <div>
-          <ui className="hidden md:flex">
-            <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+          <ui className='hidden md:flex'>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
             </Link>
-            <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            <Link href='/#about'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
             </Link>
-            <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            <Link href='/#skills'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
             </Link>
-            <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">
+            <Link href='/#projects'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>
                 Projects
               </li>
             </Link>
-            <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">
+            <Link href='/#contact'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>
                 Contact
               </li>
             </Link>
           </ui>
-          <div onClick={handleNav} className="md:hidden">
+          <div onClick={handleNav} className='md:hidden'>
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
     {/*turinary operator  */}
        <div 
-       className={nav ? "md:hidden backdrop:fixed left-0 top-0 w-full h-screen bg-black/70" : ""}
+       className={nav ? 'md:hidden backdrop:fixed left-0 top-0 w-full h-screen bg-black/70' : ''}
        >  
-        <div className={nav ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500" 
-                            : "fixed left-[-100%] top-0 p-10 ease-in duration-500"}>
+        <div className={nav ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' 
+                            : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
           <div>
-            <div className="flex w-full items-center justify-between">
+            <div className='flex w-full items-center justify-between'>
               <Image
-                src="/../public/assets/navLogo.png"
-                width="87"
-                height="35"
-                alt="/"
+                src='/../public/assets/navLogo.png'
+                width='87'
+                height='35'
+                alt='/'
               />
-              <div onClick={handleNav} className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+              <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                 <AiOutlineClose />
               </div>
             </div>
-            <div className="border-b border-gray-300 my-4">
-              <p className="w-[85%] md:w-[90%] py-4">
+            <div className='border-b border-gray-300 my-4'>
+              <p className='w-[85%] md:w-[90%] py-4'>
                 Let's build something legendary together
               </p>
             </div>
           </div>
-          <div className="py-4 flex flex-col">
-            <ul className="uppercase">
-              <Link href="/">
-                <li className="py-4 text-sm">Home</li>
+          <div className='py-4 flex flex-col'>
+            <ul className='uppercase'>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Home</li>
               </Link>
-              <Link href="/">
-                <li className="py-4 text-sm">About</li>
+              <Link href='/'>
+                <li className='py-4 text-sm'>About</li>
               </Link>
-              <Link href="/">
-                <li className="py-4 text-sm">Skills</li>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Skills</li>
               </Link>
-              <Link href="/">
-                <li className="py-4 text-sm">Projects</li>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Projects</li>
               </Link>
-              <Link href="/">
-                <li className="py-4 text-sm">Contact</li>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Contact</li>
               </Link>
             </ul>
-            <div className="pt-40">
-              <p className="uppercase tracking-widest text-[#5651e5]">
+            <div className='pt-40'>
+              <p className='uppercase tracking-widest text-[#5651e5]'>
               Let's Connect
               </p>
-              <div className="flex items-center justify-between my-4 w-full sm:[80%]">
-                <div className="rounded-full shadow-lg shadow-gray-400 p-3 marker:cursor-pointer hover:scale-105 ease-in duration-300:">
+              <div className='flex items-center justify-between my-4 w-full sm:[80%]'>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 marker:cursor-pointer hover:scale-105 ease-in duration-300:'>
                 <FaLinkedinIn />
                 </div>
-                <div className="rounded-full shadow-lg shadow-gray-400 p-3 marker:cursor-pointer hover:scale-105 ease-in duration-300:">
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 marker:cursor-pointer hover:scale-105 ease-in duration-300:'>
                 <FaGithub />
                 </div>
-                <div className="rounded-full shadow-lg shadow-gray-400 p-3 marker:cursor-pointer hover:scale-105 ease-in duration-300:">
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 marker:cursor-pointer hover:scale-105 ease-in duration-300:'>
                 <AiOutlineMail />
                 </div>
               </div>
