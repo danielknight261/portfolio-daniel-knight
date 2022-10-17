8c93940ec21305d63f642f5e9db5e0e4ddcc6b82
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2022-10-17 16:01:44 +0100

    convert of skills images to different tag

--- portfolio/components/Skills.js
+++ portfolio/components/Skills.js
@@ -32,8 +32,8 @@ const Skills = () => {
           <div className='p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
-                <Image
-                  src='/../public/assets/icons/css.png'
+                <img
+                  src='https://i.ibb.co/28tWVVN/css.png'
                   width='64px'
                   height='64px'
                   alt='/'
