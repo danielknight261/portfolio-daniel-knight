6e380c4429e32c1e612e3f84f2828236a190a5de
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-01-27 14:32:07 +0000

    removal of dark background for all projects and font colour change for title on that page

--- portfolio/pages/calculator.js
+++ portfolio/pages/calculator.js
@@ -8,7 +8,7 @@ const calculator = () => {
   return (
     <div className="w-full">
       <div className="w-screen h-[30vh] lg:h-[40vh] relative">
-        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10" />
+        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] z-10" />
         <Image
           className="absolute z-1"
           layout="fill"
@@ -17,7 +17,7 @@ const calculator = () => {
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">Open Weather API</h2>
+          <h2 className="py-2 text-gray-600">Open Weather API</h2>
           <h3></h3>
         </div>
       </div>
--- portfolio/pages/mywebsite.js
+++ portfolio/pages/mywebsite.js
@@ -8,7 +8,7 @@ const mywebsite = () => {
   return (
     <div className="w-full">
       <div className="w-screen h-[30vh] lg:h-[40vh] relative">
-        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10" />
+        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] z-10" />
         <Image
           className="absolute z-1"
           layout="fill"
@@ -17,7 +17,7 @@ const mywebsite = () => {
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">Bootcamper Social</h2>
+          <h2 className="py-2 text-gray-600">Bootcamper Social</h2>
           <h3></h3>
         </div>
       </div>
