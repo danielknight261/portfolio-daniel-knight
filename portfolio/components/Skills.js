51a688e3bb7999758dc6ab8bd76d3526eb590358
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-03-09 19:31:38 +0000

    skills updated

--- portfolio/components/Skills.js
+++ portfolio/components/Skills.js
@@ -199,23 +199,6 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          <div className='p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div className='m-auto'>
-              <img
-                  src='https://ibb.co/M7kpjTg'
-                  width='64px'
-                  height='64px'
-                  alt='/'
-                />
-              </div>
-              <div>
-                <h3 className='flex flex-col items-center justify-center'>
-                  TypeScript
-                </h3>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
